51af877892926816a349a545f3d9c0e60bf49406
Andrew Phillips <nd201175@gmail.com>
2019-05-24 12:55:20 +0000

    function to get 20 random numbers by clicking start button

--- script.js
+++ script.js
@@ -1,62 +1,63 @@
-// sequence of 20
+// sequence of 20 from start button from memory game and code insitute wwschools
 
-$(".start").click(function() {
+let order = [];
+
+document.getElementById("start").addEventListener("click", computerSequence);
+
+function computerSequence() {
+    order = [];
     for (var i = 0; i < 20; i++) {
         order.push(Math.floor(Math.random() * 4) * 1);
     }
     console.log(order);
-});
+}
 
-
-// user clicking lights 
-
-$(document).ready(function() {
-$("#green").mousedown(function(){
-  $(this).removeClass('greenButton');
-  $(this).addClass('lightGreen'); // changes to light green
-  console.log('1');
-  $("#green").mouseup(function(){
-      $(this).removeClass('lightGreen'); 
-      $(this).addClass('greenButton')//changes back
-  });
-});
-});
+// user clicking lights from code insitute module
 
 $(document).ready(function() {
-$("#blue").mousedown(function(){
-  $(this).removeClass('blueButton');
-  $(this).addClass('lightBlue'); // changes to light blue
-  console.log('2');
-  $("#blue").mouseup(function(){
-      $(this).removeClass('lightBlue'); 
-      $(this).addClass('blueButton')//changes back
-  });
-});
+    $("#green").mousedown(function() {
+        $(this).removeClass('greenButton');
+        $(this).addClass('lightGreen'); // changes to light green
+        console.log('1');
+        $("#green").mouseup(function() {
+            $(this).removeClass('lightGreen');
+            $(this).addClass('greenButton') //changes back
+        });
+    });
 });
 
 $(document).ready(function() {
-$("#yellow").mousedown(function(){
-  $(this).removeClass('yellowButton');
-  $(this).addClass('lightYellow'); // changes to light yellow
-  console.log('3');
-  $("#yellow").mouseup(function(){
-      $(this).removeClass('lightYellow'); 
-      $(this).addClass('yellowButton')//changes back
-  });
-});
+    $("#blue").mousedown(function() {
+        $(this).removeClass('blueButton');
+        $(this).addClass('lightBlue'); // changes to light blue
+        console.log('2');
+        $("#blue").mouseup(function() {
+            $(this).removeClass('lightBlue');
+            $(this).addClass('blueButton') //changes back
+        });
+    });
 });
 
 $(document).ready(function() {
-$("#red").mousedown(function(){
-  $(this).removeClass('redButton');
-  $(this).addClass('lightRed'); // changes to light red
-  console.log('4');
-  $("#red").mouseup(function(){
-      $(this).removeClass('lightRed'); 
-      $(this).addClass('redButton')//changes back
-  });
+    $("#yellow").mousedown(function() {
+        $(this).removeClass('yellowButton');
+        $(this).addClass('lightYellow'); // changes to light yellow
+        console.log('3');
+        $("#yellow").mouseup(function() {
+            $(this).removeClass('lightYellow');
+            $(this).addClass('yellowButton') //changes back
+        });
+    });
 });
+
+$(document).ready(function() {
+    $("#red").mousedown(function() {
+        $(this).removeClass('redButton');
+        $(this).addClass('lightRed'); // changes to light red
+        console.log('4');
+        $("#red").mouseup(function() {
+            $(this).removeClass('lightRed');
+            $(this).addClass('redButton') //changes back
+        });
+    });
 });
-
-
-
