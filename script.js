c2b5bfd1ce2fc16ed224012ce6cd7177aeefed56
Andrew Phillips <nd201175@gmail.com>
2019-05-28 10:43:49 +0000

    random numbers show in counter

--- script.js
+++ script.js
@@ -1,89 +1,112 @@
 $(document).ready(function() {
 
 
-// power on
+    // power on
 
     $("#power").click(function() {
         $("#count").text("--");
     });
 
 
-// sequence of 20 from start button (from memory game, code insitute, wwschools)
+    // sequence of 20 from start button (from memory game, code insitute, wwschools)
 
-    let sequence = [];
+    var sequence = [];
 
-    document.getElementById("start").addEventListener("click", computerSequence);
-
-    function computerSequence() {
+    $("#start").on("click", function() {
         sequence = [];
         for (var i = 0; i < 20; i++) {
             sequence.push(Math.floor(Math.random() * 4) + 1);
         }
         console.log(sequence);
+        $('#count').text(sequence);
+        computerColor();
+    });
+
+    // computer sequence and color
+
+
+    var userSequence = [];
+    var lightID = [];
+    var light;
+    var x;
+
+    function computerColor() {
+
+        x = setInterval(function() {
+            if (sequence == 1) {
+                light = 'lightGreen';
+                $('#green').addClass(light);
+                lightID.push(1);
+                setTimeout(function() {
+                    $('#green').removeClass(light);
+                }, 200);
+            }
+            
+            else if (sequence == 2) {
+                light = 'lightBlue';
+                $('#blue').addclass(light);
+                lightID.push(2);
+                setTimeout(function() {
+                    $('#green').removeClass(light);
+                }, 200);
+            }
+            
+            else if (sequence == 3) {
+                light = 'lightYellow';
+                $('#yellow').addClass(light);
+                lightID.push(3);
+                setTimeout(function() {
+                    $('#green').removeClass(light);
+                }, 200);
+            }
+            
+            else {
+                light = 'lightRed';
+                $('#red').addClass(light);
+                lightID.push(4);
+                setTimeout(function() {
+                    $('#green').removeClass(light);
+                }, 200);
+            }
+            
+        }, 800);
+        console.log(lightID);
     }
-    
-
-// computer sequence and color
 
 
-var userSequence = [];
-var lightID = [];
-var y = 0;
-var count = 1;
-var light;
-var x;
-
-function computerColor() {
-    
-x = setInterval(function() {
-    if (sequence == 1) {
-        light = 'lightGreen';
-        $('#green').addClass(light);
-        
-    } else if (sequence == 2) {
-        light = 'lightBlue';
-        $('#blue').addclass(light);
-        
-    } else if (sequence == 3) {
-        light = "lightYellow";
-        $('#yellow').addClass(light);
-   
-    } else {
-        light = "lightRed"; 
-        $('#red').addClass(light);
-    }
-    }, 1000);
-}
-
-// user clicking lights from code insitute module
+    // user clicking lights (from code insitute module)
 
     $("#green").mousedown(function() {
+        userSequence = [1];
         $(this).removeClass('greenButton').addClass('lightGreen'); // changes to light green
-        console.log('0');
+        console.log(userSequence);
         $("#green").mouseup(function() {
             $(this).removeClass('lightGreen').addClass('greenButton') //changes back
         });
     });
 
     $("#blue").mousedown(function() {
+        userSequence = [2];
         $(this).removeClass('blueButton').addClass('lightBlue'); // changes to light blue
-        console.log('1');
+        console.log(userSequence);
         $("#blue").mouseup(function() {
             $(this).removeClass('lightBlue').addClass('blueButton') //changes back
         });
     });
 
     $("#yellow").mousedown(function() {
+        userSequence = [3];
         $(this).removeClass('yellowButton').addClass('lightYellow'); // changes to light yellow
-        console.log('2');
+        console.log(userSequence);
         $("#yellow").mouseup(function() {
             $(this).removeClass('lightYellow').addClass('yellowButton') //changes back
         });
     });
 
     $("#red").mousedown(function() {
+        userSequence = [4];
         $(this).removeClass('redButton').addClass('lightRed'); // changes to light red
-        console.log('3');
+        console.log(userSequence);
         $("#red").mouseup(function() {
             $(this).removeClass('lightRed').addClass('redButton') //changes back
         });
