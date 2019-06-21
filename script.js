af858484ecee504f643d5badf11cea1386012976
Andrew Phillips <nd201175@gmail.com>
2019-06-21 09:05:56 +0000

    simon plays complete sequence of 20 numbers

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-    var on = false;
     var sequence = [];
     var cpuSequence = [];
     var userSequence = [];
-    var success = false;
-    var turn = 0;
- //   var cpuTurn;
-    //   var count = 1;
-
-    //    var buttonClicked = true;
+    var success;
+    var turn;
+    var cpuTurn;
+    var playerTurn;
+    var bright;
+    var playSequence;
+    var win;
 
     // power on/off
 
@@ -17,192 +17,193 @@ $(document).ready(function() {
 
     function powerButton() {
         var x = document.getElementById("power").classList.contains("change");
-        console.log(x);
         if (x == false) {
-            console.log('on');
-            on = true;
+            playerTurn = true;
             $('#power').addClass("change");
             powerOn();
         }
         else {
-            console.log('off');
-            on = false;
+            playerTurn = false;
+            clearInterval(playSequence);
             $('#power').removeClass("change");
             powerOff();
         }
     }
 
     function powerOn() {
-        if (on == true) {
-            $("#count").html('0');
-            sequence = [];
-            turn = 0;
-            userSequence = [];
-            cpuSequence = [];
-            startSimon();
-            document.getElementById("start").disabled = false;
-            document.getElementById("inner").disabled = false;
-            //         $("#start").disabled = false;
-            //      x = false;
-        }
-        else {
-            restart();
-        }
+        $("#count").html('--');
+        document.getElementById("start").disabled = false;
+        document.getElementById("inner").disabled = false;
+        $("#start").on("click", function() {
+            $("#count").html('1');
+            if (playerTurn || success) {
+                play();
+            }
+        });
     }
 
     function powerOff() {
         $('.colorButton').removeClass('bright'),
             $("#count").html('');
+
         document.getElementById("start").disabled = true;
         document.getElementById("inner").disabled = true;
-        //           $("#start").disabled = true;
+        window.location.reload();
     }
-
-    // press start button to begin
-    function startSimon() {
-        if (on == true) {
-            $("#start").on("click", function() {
-                $("#count").html('1');
-                //        if (on || success) {
-                play();
-            });
-        }
-    }
-
-
 
     // start play function by creating sequence of 20 from start button (from memory game, code insitute, wwschools)
 
     function play() {
-        //      sequence = [];
-        //       var count = 1;
-        //       success = false;
-        //        userSequence = [];
-        //        cpuSequence = [];
-        //        $("#count").html(turn);
-        //        turn = 0;
-
+        playSequence = 0;
+        sequence = [];
+        userSequence = [];
+        bright = 0;
+        turn = 1;
+        success = true;
+        win = false;
+        $("#count").html(turn);
         for (var i = 0; i < 20; i++) {
             sequence.push(Math.floor(Math.random() * 4) + 1);
         }
-
         console.log(sequence);
-
-        setTimeout(makeBright, 200);
-
+        cpuTurn = true;
+        playSequence = setInterval(makeBright, 1000);
     }
 
     function makeBright() {
-        //       if (success == true) {
-        if (sequence[turn] == 1) {
-            cpuSequence.push(1);
-            console.log(cpuSequence);
-            $('#green').addClass('bright'),
-                setTimeout(function() {
-                    $('#green').removeClass('bright');
-                }, 800);
-        }
-        else if (sequence[turn] == 2) {
-            cpuSequence.push(2);
-            console.log(cpuSequence);
-            $('#blue').addClass('bright'),
-                setTimeout(function() {
-                    $('#blue').removeClass('bright');
-                }, 800);
-        }
-        else if (sequence[turn] == 3) {
-            cpuSequence.push(3);
-            console.log(cpuSequence);
-            $('#yellow').addClass('bright'),
-                setTimeout(function() {
-                    $('#yellow').removeClass('bright');
-                }, 800);
-        }
-        else if (sequence[turn] == 4) {
-            cpuSequence.push(4);
-            console.log(cpuSequence);
-            $('#red').addClass('bright'),
-                setTimeout(function() {
-                    $('#red').removeClass('bright');
-                }, 800);
+        playerTurn = false;
+        if (bright == turn) {
+            clearInterval(playSequence);
+            cpuTurn = false;
+            playerTurn = true;
         }
 
-        userTurn();
+        if (cpuTurn) {
+            $('.colorButton').removeClass('bright');
+            setTimeout(function() {
+                if (sequence[turn] == 1) {
+                    cpuSequence.push(1);
+                    turn++;
+                    console.log(cpuSequence);
+                    $('#green').addClass('bright'),
+                        setTimeout(function() {
+                            $('#green').removeClass('bright');
+                        }, 800);
+                }
+                else if (sequence[turn] == 2) {
+                    cpuSequence.push(2);
+                    console.log(cpuSequence);
+                    turn++;
+                    $('#blue').addClass('bright'),
+                        setTimeout(function() {
+                            $('#blue').removeClass('bright');
+                        }, 800);
+                }
+                else if (sequence[turn] == 3) {
+                    cpuSequence.push(3);
+                    console.log(cpuSequence);
+                    turn++;
+                    $('#yellow').addClass('bright'),
+                        setTimeout(function() {
+                            $('#yellow').removeClass('bright');
+                        }, 800);
+                }
+                else if (sequence[turn] == 4) {
+                    cpuSequence.push(4);
+                    console.log(cpuSequence);
+                    turn++;
+                    $('#red').addClass('bright'),
+                        setTimeout(function() {
+                            $('#red').removeClass('bright');
+                        }, 800);
+                }
+            }, 200);
+        }
+    }
 
-        //        }
-    };
+    // user clicking lights (from code insitute module)
+//    function userTurn() {
+//        bright = 0;
+//        console.log(turn);
+//        userSequence = [];
+        if (playerTurn == true && success == true) {
 
-    function userTurn() {
-        //        if (count == turn) {
-        //         if (success == true) {
-
-        // user clicking lights (from code insitute module)
-
-
-        $("#green").mousedown(function() {
-            $(this).addClass('bright'); // changes to light green
-            userSequence.push(1);
-            console.log(userSequence);
-            $("#green").mouseup(function() {
-                $(this).removeClass('bright'); //changes back
-                compareArrays();
-            });
-
-        });
-
-        $("#blue").mousedown(function() {
-            $(this).addClass('bright'); // changes to light blue
-            userSequence.push(2);
-            console.log(userSequence);
-            $("#blue").mouseup(function() {
-                $(this).removeClass('bright'); //changes 
-                compareArrays();
-            });
-
-        });
-
-        $("#yellow").mousedown(function() {
-            userSequence.push(3);
-            console.log(userSequence);
-            $(this).addClass('bright'); // changes to light yellow
-
-            $("#yellow").mouseup(function() {
-                $(this).removeClass('bright'); //changes back
-                compareArrays();
-            });
-
-        });
-
-        $("#red").mousedown(function() {
-            userSequence.push(4);
-            console.log(userSequence);
-            $(this).addClass('bright'); // changes to light red
-
-            $("#red").mouseup(function() {
+            $("#green").mousedown(function() {
+                $(this).addClass('bright'); // changes to light green
+                userSequence.push(1);
+                //       bright++;
+                console.log(userSequence);
+                $("#green").mouseup(function() {
                     $(this).removeClass('bright'); //changes back
                     compareArrays();
-                
+                });
             });
-        });
+
+            $("#blue").mousedown(function() {
+                userSequence.push(2);
+                //       bright++;
+                console.log(userSequence);
+                $(this).addClass('bright'); // changes to light blue
+                $("#blue").mouseup(function() {
+                    $(this).removeClass('bright'); //changes 
+                    compareArrays();
+                });
+            });
+
+            $("#yellow").mousedown(function() {
+                userSequence.push(3);
+                //     bright++;
+                console.log(userSequence);
+                $(this).addClass('bright'); // changes to light yellow
+                $("#yellow").mouseup(function() {
+                    $(this).removeClass('bright'); //changes back
+                    compareArrays();
+                });
+            });
+
+            $("#red").mousedown(function() {
+                userSequence.push(4);
+                //        bright++;
+                console.log(userSequence);
+                $(this).addClass('bright'); // changes to light red
+                $("#red").mouseup(function() {
+                    $(this).removeClass('bright'); //changes back
+                    compareArrays();
+                });
+            });
+            bright++;
+        }
+        playerTurn = false;
+
     }
 
     function compareArrays() {
-        if (userSequence[userSequence.length - 1] == cpuSequence[cpuSequence.length - 1]) {
-            console.log("correct");
-            $("#count").html(turn + 1);
-            success = true;
-            setTimeout(makeBright(), 500);
-            turn++;
-            console.log(turn);
-            $("#count").html(turn);
-        }
-        else if (userSequence.length == 3 && success == true) {
-            win();
+        console.log(userSequence.length);
+        console.log(cpuSequence.length);
+        console.log(bright);
+        console.log(turn);
+        if (userSequence.length == cpuSequence.length && bright == turn) {
+            if (userSequence.join() == cpuSequence.join()) {
+                if (turn == 20) {
+                    win();
+                    console.log("correct");
+                }
+                else {
+
+                    bright = 0;
+                    success = true;
+                    userSequence = [];
+                    cpuSequence = [];
+                    console.log(turn);
+                    $("#count").html(turn);
+                    setTimeout(makeBright(), 500);
+                }
+            }
         }
         else {
             console.log("wrong");
             success = false;
-            setTimeout(end(), 500);
+            end();
         }
     }
 
@@ -213,14 +214,17 @@ $(document).ready(function() {
         turn = 0;
         $(".colorButton").addClass('bright');
         $("#count").html('LOST');
-        setTimeout(restart, 3000);
+        //      setTimeout(restart, 3000);
     }
 
     function restart() {
-        $("#count").html('0');
-        $(".colorButton").removeClass('bright');
-        console.log(cpuSequence);
-        console.log(userSequence);
+        //        window.location.reload();
+        powerOn();
+        //       $("#count").html('0');
+        //       $(".colorButton").removeClass('bright');
+        //        console.log(cpuSequence);
+        //        console.log(userSequence);
+        //        cpuTurn = true;
     }
 
     function win() {
